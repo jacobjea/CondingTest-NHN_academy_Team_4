9e609a3af21d6e08d122ea9d86e1e8e21633a2d8
jeseungwook <jsw992@gmail.com>
2024-09-03 21:46:19 +0900

    스택 / 실버4 / 25분 / 제승욱

--- SeungWook/스택.java
+++ SeungWook/스택.java
@@ -5,59 +5,67 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class 스택 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         Stack<Integer> stack = new Stack<>();
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             String s = br.readLine();
             if (s.contains(" ")) {
                 String[] command = s.split(" ");
                 stack.push(Integer.parseInt(command[1]));
-            }
-            else if (s.equals("top")) {
-                System.out.println(stack.top());
-            }
-            else if (s.equals("size")) {
+            } else if (s.equals("top")) {
+                if (stack.isEmpty()) {
+                    System.out.println("-1");
+                }
+                else{
+                    System.out.println(stack.top());
+                }
+            } else if (s.equals("size")) {
                 System.out.println(stack.size());
-            }
-            else if (s.equals("empty")) {
-                String message = stack.isEmpty() ? "1":"0";
+            } else if (s.equals("empty")) {
+                String message = stack.isEmpty() ? "1" : "0";
                 System.out.println(message);
+            } else if (s.equals("pop")) {
+                if (stack.isEmpty()) {
+                    System.out.println("-1");
+                }
+                else{
+                    System.out.println(stack.pop());
+                }
             }
-            
         }
     }
 }
 
 @SuppressWarnings("unchecked")
-class Stack<E>{
+class Stack<E> {
     E[] list;
     int index = 0;
 
-    public Stack(){
-        list = (E[]) new Object[10000];
+    public Stack() {
+        list = (E[]) new Object[100000];
     }
 
-    public void push(E e){
+    public void push(E e) {
         list[index++] = e;
     }
 
-    public E pop(){
+    public E pop() {
         return list[--index];
     }
 
-    public int size(){
+    public int size() {
         return index;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return index == 0;
     }
 
-    public E top(){
-        return list[index-1];
+    public E top() {
+        return list[index - 1];
     }
 }
