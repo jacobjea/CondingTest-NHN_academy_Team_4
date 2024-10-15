82c1c7b6b2365bc30099f22a5b8f031a536cce88
jacobjea <jsw992@gmail.com>
2024-10-16 00:25:29 +0900

    대소문자_바꾸기 / 브론즈 5 / 5분 / 제승욱

---
+++ SeungWook/대소문자_바꾸기.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class 대소문자_바꾸기 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String  br.readLine()
+        
+    }
+}
