0bc9ae0768fef220d48c6fddfcace61a3e6ebe0d
00dbgpdnjs <81615868+00dbgpdnjs@users.noreply.github.com>
2024-10-16 21:46:46 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 1 / 브론즈5 / 5분 / 유혜원

---
+++ HyeWon/알고리즘_수업_알고리즘의_수행_시간_1.java
@@ -0,0 +1,14 @@
+package implementation;
+
+import java.util.Scanner;
+public class 알고리즘_수업_알고리즘의_수행_시간_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		sc.new
+		System.out.println(1);
+		System.out.println(0);
+		
+	}
+
+}
