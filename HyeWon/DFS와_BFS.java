489311f0a824514d0f65c6f30791734ade534bfd
00dbgpdnjs <81615868+00dbgpdnjs@users.noreply.github.com>
2024-11-10 15:00:46 +0900

    DFS와 BFS / 실버2 / 1시간 / 유혜원

--- HyeWon/DFS와_BFS.java
+++ HyeWon/DFS와_BFS.java
@@ -1,59 +1,134 @@
-import java.util.*;
-public class DFS와_BFS {
-    static boolean[][] map;
-    static boolean[] chk;
-    static Queue<Integer> q = new LinkedList<>();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int start = sc.nextInt();
+package search.bfsDfs;
 
-        map = new boolean[n+1][n+1];
-        chk = new boolean[n+1];
+/*
+1:17
+1. 아이디어
+List<Integer>[] vertex = new List[n+1];
+for n
+	vertext[i] = new LinkedList<>[];
 
-        for(int i=0; i<m; i++) {
-            int v1 = sc.nextInt();
-            int v2 = sc.nextInt();
-            map[v1][v2] =true;
-            map[v2][v1] =true;
-        }
+for m
+	v1, v2 = sc
+	vertext[v1].add(v2)
+	vertext[v2].add(v1)
 
-        // for(int i=0; i<map.length; i++) System.out.println(Arrays.toString(map[i]));
+boolean [n+1] visited
 
-        dfs(start);
-        System.out.println();
-        chk = new boolean[n+1];
-        bfs(start);
-    }
+bfs(start):
+	start 방문 처리 
+	인큐
+	while(큐 빌때까지)
+		currV = dequeue
+		임시 배열 초기화
+		for vertext[currV]
+			if 방문 안했으면
+				방문 처리
+				배열에 넣기
+		배열 오름 차순 정렬
+		배열 차례대로 출력하고 인큐
 
-    static public void dfs(int start) {
-        chk[start] = true;
-        System.out.print(start+" ");
+dfs(v):
+	start 방문 처리
+	//스택 푸쉬
+	//while(스택 빌떄까지)
+	//	currV = 팝
+//		min = n
+		for graph[v]
+			if 방문 안했으면
+				dfs(nv)
 
-        // List<Integer> list = new ArrayList<>();
-        for(int i=0; i<map.length; i++) {
-            if (map[start][i] && start!=i && !chk[i]) dfs(i);
-        }
-    }
-	
-	public static void bfs(int start) {
-		q.add(start);
-		chk[start] = true;
+2. 시간 복잡도
+v+e = 1000+10000 = 11000
 		
-		while(!q.isEmpty()) {
-			
-			start = q.poll();
-            System.out.print(start+" ");
-			
-			for(int i = 1 ; i<map.length ; i++) {
-				if(map[start][i] && !chk[i]) {
-					q.add(i);
-					chk[i] = true;
-				}
-			}
+
+ */
+import java.io.*;
+import java.util.*;
+
+public class DFS와_BFS {
+	static List<Integer>[] graph;
+	static int n;
+	static boolean [] visited;
+	static StringBuilder resultForDfs = new StringBuilder();
+
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] line = br.readLine().split(" ");
+		n = Integer.parseInt(line[0]);
+		int m = Integer.parseInt(line[1]);
+		int startV = Integer.parseInt(line[2]);
+		
+		graph = new List[n+1];
+		for(int i=1; i<n+1; i++) {
+			graph[i] = new LinkedList<>(); 
 		}
 		
+		for(int i=0; i<m; i++) {
+			line = br.readLine().split(" ");
+			int v1 = Integer.parseInt(line[0]);
+			int v2 = Integer.parseInt(line[1]);
+			
+			graph[v1].add(v2);
+			graph[v2].add(v1);
+		}
+
+		for(int i=1; i<n+1; i++) {
+			Collections.sort(graph[i]);
+		}
+		
+		visited = new boolean [n+1];
+		dfs(startV);
+		System.out.println(resultForDfs);
+
+		bfs(startV);
+	}
+
+	private static void dfs(int v) {
+		visited[v] = true;
+		resultForDfs.append(v).append(" ");
+		for(int nv : graph[v]) {
+			if(!visited[nv]) {
+				dfs(nv);
+			}
+		}
+	}
+
+	private static void bfs(int startV) {
+		boolean [] visited = new boolean [n+1];
+		visited[startV] = true;
+		Queue<Integer> q = new LinkedList<>();
+		q.add(startV);
+		
+		StringBuilder result = new StringBuilder();
+		result.append(startV).append(" ");
+		
+		while(!q.isEmpty()) {
+			int currV = q.poll();
+//			int[] tmp = new int[graph[currV].size()];
+//			List<Integer> tmp = new ArrayList<>();
+			for(int i=0; i<graph[currV].size(); i++) {
+				int nv = graph[currV].get(i);
+				if(!visited[nv]) {
+					visited[nv] = true;
+//					tmp[i] = nv;
+//					tmp.add(nv);
+
+					q.add(nv);
+					result.append(nv).append(" ");
+				}
+			}
+//			Arrays.sort(tmp);
+			
+//			Collections.sort(tmp);
+//			for(int v : tmp) {
+//				q.add(v);
+//				result.append(v).append(" ");
+//			}
+		}
+		
+		System.out.println(result);
 		
 	}
+
 }
