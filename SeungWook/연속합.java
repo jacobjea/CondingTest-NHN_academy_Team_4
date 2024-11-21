0f72526b3c167f4c4a8325f7de3a73634e4715aa
jacobjea <jsw992@gmail.com>
2024-11-21 21:52:15 +0900

    연속합 / 실버 2 / 45분 / 제승욱

---
+++ SeungWook/연속합.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class 연속합 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] array = new int[n];
+        for(int i = 0; i< n; i++){
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        dp[0] = array[0];
+
+        int max = array[0];
+        for(int i =1; i<n; i++){
+            dp[i] = Math.max(dp[i-1] + array[i], array[i]);
+            max = Math.max(max, dp[i]); 
+        }
+        System.out.println(max);
+    }
+}
